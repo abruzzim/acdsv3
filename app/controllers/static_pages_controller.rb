7806a6585b30bb6ed1e3cebf72b8b883a3416582
abruzzim <abruzzim@framework-solutions.com>
2015-05-11 00:49:36 -0400

    Add Treatment Guidelines Placeholder Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,27 @@
 class StaticPagesController < ApplicationController
+
 	def home
+		puts "%STATIC_PAGES_CONTROLLER-I-TRACE, 'home' called."
 	end
+
+	def asthmaticus
+		puts "%STATIC_PAGES_CONTROLLER-I-TRACE, 'asthmaticus' called."
+	end
+
+	def dka
+		puts "%STATIC_PAGES_CONTROLLER-I-TRACE, 'dka' called."
+	end
+
+	def epilepticus
+		puts "%STATIC_PAGES_CONTROLLER-I-TRACE, 'epilepticus' called."
+	end
+
+	def sepsis
+		puts "%STATIC_PAGES_CONTROLLER-I-TRACE, 'sepsis' called."
+	end
+
+	def tbi
+		puts "%STATIC_PAGES_CONTROLLER-I-TRACE, 'tbi' called."
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
-  get    'signup' => 'users#new'
-  get    'login'  => 'sessions#new'
-  post   'login'  => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  get    'asthmaticus' => 'static_pages#asthmaticus'
+  get    'dka'         => 'static_pages#dka'
+  get    'epilepticus' => 'static_pages#epilepticus'
+  get    'sepsis'      => 'static_pages#sepsis'
+  get    'tbi'         => 'static_pages#tbi'
+  get    'signup'      => 'users#new'
+  get    'login'       => 'sessions#new'
+  post   'login'       => 'sessions#create'
+  delete 'logout'      => 'sessions#destroy'
   resources :users
 end
