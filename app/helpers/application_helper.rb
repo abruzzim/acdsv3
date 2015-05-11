07cd678dda0f96cc3bd5844765f3a8d9c4e4a7ad
abruzzim <abruzzim@framework-solutions.com>
2015-05-11 00:13:37 -0400

    Add Footer & Navbar Dropdown Items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 	# Return the full title on a per-page basis.
-	def full_title(page_title = '')				# Method def, optional arg
-		base_title = "ACDS" 					# Variable assignment
-		if page_title.empty?					# Boolean test
-			base_title							# Implicit return
+	def full_title(page_title = '') 		# Method def, optional arg
+		base_title = "ACDS" 							# Variable assignment
+		if page_title.empty? 							# Boolean test
+			base_title 											# Implicit return
 		else
-			"#{page_title} | #{base_title}" 	# String interpolation
+			"#{page_title} | #{base_title}" # String interpolation
 		end
 	end
 end
