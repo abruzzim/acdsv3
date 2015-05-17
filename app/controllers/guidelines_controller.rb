b7351db0b8c1ab722743b78f11d242bd30c9fbed
abruzzim <abruzzim@framework-solutions.com>
2015-05-17 10:04:18 -0400

    Add Placeholder Guideline Pages

--- app/controllers/guidelines_controller.rb
+++ app/controllers/guidelines_controller.rb
@@ -1,7 +1,23 @@
 class GuidelinesController < ApplicationController
 
+	def asthmaticus
+		puts "%GUIDELINES_CONTROLLER-I-TRACE, 'asthmaticus' called."
+	end
+
+	def dka
+		puts "%GUIDELINES_CONTROLLER-I-TRACE, 'dka' called."
+	end
+
+	def epilepticus
+		puts "%GUIDELINES_CONTROLLER-I-TRACE, 'epilepticus' called."
+	end
+
 	def sepsis
 		puts "%GUIDELINES_CONTROLLER-I-TRACE, 'sepsis' called."
+	end
+
+	def tbi
+		puts "%GUIDELINES_CONTROLLER-I-TRACE, 'tbi' called."
 	end
 
 end
